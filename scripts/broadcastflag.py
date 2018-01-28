5944ea5353be9c7746aab394d043b3724493c6c8
Lars <eykomm@users.noreply.github.com>
2018-01-28 11:24:50 +0100

    initial script and ideas

---
+++ scripts/broadcastflag.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#small script to receive current ip address and send a message to broadcast address of the network.
+
+import socket
+hostname=socket.gethostname()
+IPAddr=socket.gethostbyname(hostname)
+print("Your Computer Name is:"+hostname)
+print("Your Computer IP Address is:"+IPAddr)
+
+# flag sent to broadcast address
+from socket import *
+cs = socket(AF_INET, SOCK_DGRAM)
+cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+cs.sendto('Flag1{base64=}', ('<broadcast>', 54545))
+
