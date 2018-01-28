34bc48fb635b4f1e1006b94fcd7f8f3d13f7cb2e
root <root@localhost.localdomain>
2018-01-28 05:46:19 -0500

    sh

---
+++ scripts/bash_repeat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true;
+	do
+	python /root/Projects/git/quali_ctf/scripts/broadcastflag.py;
+	sleep 10;
+done
--- scripts/broadcastflag.py
+++ scripts/broadcastflag.py
@@ -2,11 +2,11 @@
 
 #small script to receive current ip address and send a message to broadcast address of the network.
 
-import socket
-hostname=socket.gethostname()
-IPAddr=socket.gethostbyname(hostname)
-print("Your Computer Name is:"+hostname)
-print("Your Computer IP Address is:"+IPAddr)
+# import socket
+# hostname=socket.gethostname()
+# IPAddr=socket.gethostbyname(hostname)
+# print("Your Computer Name is:"+hostname)
+# print("Your Computer IP Address is:"+IPAddr)
 
 # flag sent to broadcast address
 from socket import *
@@ -14,4 +14,5 @@ cs = socket(AF_INET, SOCK_DGRAM)
 cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 cs.sendto('Flag1{base64=}', ('<broadcast>', 54545))
+print("flag sent")
 
